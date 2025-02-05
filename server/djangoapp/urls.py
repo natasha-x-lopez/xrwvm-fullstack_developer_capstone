b5b2a55aa2a7df6a3423241ee0d81ab229b25776
Natasha <natasha.x.lopez@icloud.com>
2025-02-05 08:41:34 -0500

    Updated URL patterns and view functions

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,20 +3,24 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
+from .views import get_cars
 
 app_name = 'djangoapp'
 urlpatterns = [
     # path for registration
-    path('register', views.registration, name='register'),
+    path('register/', views.registration, name='register'),
 
     # path for login
-    path(route='login', view=views.login_user, name='login'),
+    path(route='login/', view=views.login_user, name='login'),
 
     # path for logout
-    path(route='logout', view=views.logout_request, name='logout'),
+    path(route='logout/', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
 
     # path for add a review view
+
+    # path for get cars
+    path(route='get_cars/', view=views.get_cars, name ='getcars'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,16 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://natashaxlope-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://natashaxlope-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = [
+    'localhost',
+    'natashaxlope-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'natashaxlope-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://natashaxlope-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://natashaxlope-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
